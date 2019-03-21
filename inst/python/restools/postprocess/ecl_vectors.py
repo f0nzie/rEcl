26626988d979700a12d16250a7da67be11222619
f0nzie zbook G4 <alfonso.reyes@oilgainsanalytics.com>
2019-03-21 18:56:47 -0500

    new module to store Eclipse keywords

---
+++ inst/python/restools/postprocess/ecl_vectors.py
@@ -0,0 +1,17 @@
+
+
+ecl_vectors = ['COPR', 'COPT', 'CWFR', 'CWIR', 'CWPR', 'CWPT', 'FGIR', 'FGIT',
+               'FGLIR', 'FGOR', 'FGORH', 'FGPR', 'FGPT', 'FLPR', 'FLPT',
+               'FMCTP', 'FMWWO', 'FMWWT', 'FODEN', 'FOE', 'FOIP', 'FOPR', 'FOPRF',
+               'FOPRH', 'FOPRS', 'FOPT', 'FOPTH', 'FPR', 'FVIR', 'FVIT', 'FVPR',
+               'FVPT', 'FWCT', 'FWCTH', 'FWIP', 'FWIR', 'FWIT', 'FWPR', 'FWPT',
+               'GGOR', 'GGPR', 'GGPT', 'GOPR', 'GOPT', 'GVIR', 'GVIT', 'GVPR',
+               'GVPT', 'GWCT', 'GWIR', 'GWPR', 'MSUMLINS', 'RGPV', 'RHPV', 'ROE',
+               'ROEW', 'ROPV', 'ROSAT', 'RPR', 'RRPV', 'RWPV', 'TCPU', 'TIME',
+               'WBHP', 'WBHPH', 'WBP', 'WBP4', 'WBP9', 'WGIR', 'WGIT', 'WGLIR',
+               'WGOR', 'WGORH', 'WGPR', 'WGPRH', 'WGPTH', 'WLPR', 'WLPRH',
+               'WLPT', 'WLPTH', 'WMCON', 'WMCTL', 'WOPR', 'WOPRH', 'WOPT',
+               'WOPTH', 'WPI', 'WTHP', 'WTICIW1', 'WTICIW2', 'WTIRIW1',
+               'WTIRIW2', 'WTPCIW1', 'WTPCIW2', 'WTPRIW1', 'WTPRIW2', 'WWCT',
+               'WWCTH', 'WWIR', 'WWIRH', 'WWIT', 'WWITH', 'WWPR', 'WWPRH',
+               'WWPT', 'WWPTH', 'YEARS']
